2c3c28e639ac7ec585efb1c3da2fd552726f4238
Ivan Lencina <colo9311@gmail.com>
2021-09-12 21:44:25 -0300

    React fragment added. Looping on a fake todo list.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 // import './App.css';
 
+import React from "react";
+
+const todos = [
+  { text: 'Cortar cebolla', completed: false },
+  { text: 'Pelar papas', completed: false },
+  { text: 'Salir a correr', completed: false }
+]
+
 function App(props) {
   return (
-    <TodoCounter />
-    <TodoSearch />
-    <TodoList>
-      <TodoItem />
-    </TodoList>
-    <CreateTodoButton />
+    // React.Fragments inserts an Invisible tag in order to avoid using a div.
+    <React.Fragment>
+      <TodoCounter />
+      <TodoSearch />
+      <TodoList>
+        { todos.map(todo => (
+          <TodoItem />
+        )) }
+      </TodoList>
+      <CreateTodoButton />
+    </React.Fragment>
+    
   );
 }
 
