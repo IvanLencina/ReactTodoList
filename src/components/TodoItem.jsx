f92a4d16169b53edf7cf327f252cf826760bc3f4
Ivan Lencina <colo9311@gmail.com>
2021-09-13 00:23:53 -0300

    Events added to the TodoItem component.

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -2,15 +2,23 @@ import React from 'react';
 import '../styles/TodoItem.css';
 
 function TodoItem(props) {
+  const onComplete = () => {
+    alert('Ya completaste el TODO: ' + props.text);
+  }
+
+  const onDelete = () => {
+    alert('Borraste el TODO: ' + props.text);
+  }
+
   return (
     <li className="todo_item">
-      <span className={`icon icon-check ${props.completed && 'icon-check--active'}`}>
+      <span className={`icon icon-check ${props.completed && 'icon-check--active'}`} onClick={onComplete}>
         √
       </span>
       <p className={`todo_item-p ${props.completed && 'todo_item-p--complete'}`}>
         {props.text}
       </p>
-      <span className="icon icon-delete">
+      <span className="icon icon-delete" onClick={onDelete}>
         X
       </span>
     </li>
