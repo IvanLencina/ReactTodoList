a91099c84eb274d8f340f62da7a4ae0fa1e4abf3
Ivan Lencina <colo9311@gmail.com>
2021-09-12 21:36:29 -0300

    Adding basic component structure.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <App msg="Prueba de props">
-    Buenassssssss
-  </App>,
+  <App />,
   document.getElementById('root')
 );
 
