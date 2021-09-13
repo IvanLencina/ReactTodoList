d0755d69c7d9d63242d7b3b2785f76a2ddd84f38
Ivan Lencina <colo9311@gmail.com>
2021-09-13 01:38:01 -0300

    Comments added.

--- src/App.js
+++ src/App.js
@@ -14,12 +14,20 @@ const defaultTodos = [
 ];
 
 function App(props) {
+  // Set todos as an app level state because we will be connecting that with other components.
   const [ todos, setTodos] = React.useState(defaultTodos);
+  // The same for the searchValue state. It was moved from TodoSearch component because we will performing
+  // different actions with that shared state, eg: filtering by input search in the renderer list of todos.
   const [ searchValue, setSearchValue ] = React.useState('');
 
+  // Calculating the completed and total todos to pass them to the TodoCounter component.
+  // these properties are being updated every todo state change.
   const completedTodosCount = todos.filter(todo => !!todo.completed).length;
   const totalTodos = todos.length;
 
+  // Filter implementation. If the user didn't write any value, the filtered array will have the default todos state value.
+  // if not, it will filter by the searchValue through all the todos by their text property.
+  // NOTE that we updated the TodoList children to loop over the searchedTodos array insted the original todos' state.
   let searchedTodos = [];
 
   if (!searchValue.length >= 1) {
