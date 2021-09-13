15e8696e1ac0fb182dc6f9e017b01c1223c5e689
Ivan Lencina <colo9311@gmail.com>
2021-09-12 23:36:24 -0300

    Components created.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 // import './App.css';
 
 import React from "react";
+import { CreateTodoButton } from "./components/CreateTodoButton";
+import { TodoCounter } from "./components/TodoCounter";
+import { TodoItem } from "./components/TodoItem";
+import { TodoList } from "./components/TodoList";
+import { TodoSearch } from "./components/TodoSearch";
 
 const todos = [
   { text: 'Cortar cebolla', completed: false },
@@ -13,15 +18,17 @@ function App(props) {
     // React.Fragments inserts an Invisible tag in order to avoid using a div.
     <React.Fragment>
       <TodoCounter />
+
       <TodoSearch />
+      
       <TodoList>
-        { todos.map(todo => (
-          <TodoItem />
+        { todos.map((todo, index) => (
+          <TodoItem key={index} text={todo.text} />
         )) }
       </TodoList>
+
       <CreateTodoButton />
     </React.Fragment>
-    
   );
 }
 
