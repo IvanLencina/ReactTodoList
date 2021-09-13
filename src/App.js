b7d268df773fa1bdbabd359a781043eab1a8813c
Ivan Lencina <colo9311@gmail.com>
2021-09-13 01:04:46 -0300

    Created todos state and initializing them with the mocked array.

--- src/App.js
+++ src/App.js
@@ -7,13 +7,14 @@ import { TodoItem } from "./components/TodoItem";
 import { TodoList } from "./components/TodoList";
 import { TodoSearch } from "./components/TodoSearch";
 
-const todos = [
+const defaultTodos = [
   { text: "Cortar cebolla", completed: true },
   { text: "Pelar papas", completed: false },
   { text: "Salir a correr", completed: false },
 ];
 
 function App(props) {
+  const [ todos, setTodos] = React.useState(defaultTodos);
   const [ searchValue, setSearchValue ] = React.useState('');
 
   return (
