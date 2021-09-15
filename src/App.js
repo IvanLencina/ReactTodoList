9654ee8d2b6aecaa20bf0504d268df5b23166757
Ivan Lencina <colo9311@gmail.com>
2021-09-15 01:25:50 -0300

    Save todos strategy implemented. Stop using default todos and now using the todos from localStorage if they exist.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { AppUI } from "./AppUI";
 
-const defaultTodos = [
-  { text: "Cortar cebolla", completed: true },
-  { text: "Pelar papas", completed: false },
-  { text: "Salir a correr", completed: false },
-];
-
 function App(props) {
+  const localStorageTodos = localStorage.getItem('TODOS_V1');
+  let parsedTodos;
+
+  if (!localStorageTodos) {
+    localStorage.setItem('TODOS_V1', JSON.stringify([]));
+    parsedTodos = [];
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+
   // Set todos as an app level state because we will be connecting that with other components.
-  const [ todos, setTodos] = React.useState(defaultTodos);
+  const [ todos, setTodos] = React.useState(parsedTodos);
   // The same for the searchValue state. It was moved from TodoSearch component because we will performing
   // different actions with that shared state, eg: filtering by input search in the renderer list of todos.
   const [ searchValue, setSearchValue ] = React.useState('');
@@ -35,6 +39,12 @@ function App(props) {
     })
   }
 
+  const saveTodos = (newTodos) => {
+    localStorage.setItem('TODOS_V1', JSON.stringify(newTodos));
+
+    setTodos(newTodos);
+  }
+
   const toggleTodoCompletion = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text);
     
@@ -42,12 +52,14 @@ function App(props) {
     newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
 
     setTodos(newTodos);
+    saveTodos(newTodos);
   }
 
   const deleteTodo = (text) => {
     const newTodos = todos.filter(todo => todo.text !== text);
 
     setTodos(newTodos);
+    saveTodos(newTodos);
   }
 
   return (
