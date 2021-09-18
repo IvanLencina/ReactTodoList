190a43e2fee217edf112e717fb7b512d6675a0f3
Ivan Lencina <colo9311@gmail.com>
2021-09-18 20:50:52 -0300

    AppUI component refactor: Using TodoContext.Consumer to retrieve the props. RenderProps applied.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { AppUI } from "./AppUI";
 import { TodoProvider } from "./TodoContext";
-function App(props) {
-  
 
+function App(props) {
   return (
     <TodoProvider>
       <AppUI />
--- src/AppUI.jsx
+++ src/AppUI.jsx
@@ -4,46 +4,35 @@ import { TodoCounter } from "./components/TodoCounter";
 import { TodoItem } from "./components/TodoItem";
 import { TodoList } from "./components/TodoList";
 import { TodoSearch } from "./components/TodoSearch";
+import { TodoContext } from "./TodoContext";
 
-function AppUI({
-  loading,
-  error,
-  completedTodosCount,
-  totalTodos,
-  searchValue,
-  setSearchValue,
-  searchedTodos,
-  toggleTodoCompletion,
-  deleteTodo,
-}) {
+
+function AppUI() {
   return (
     // React.Fragments inserts an Invisible tag in order to avoid using a div.
     <React.Fragment>
-      <TodoCounter
-        completedTodosCount={completedTodosCount}
-        totalTodos={totalTodos}
-      />
+      <TodoCounter />
+      <TodoSearch/>
 
-      <TodoSearch
-        searchValue={searchValue}
-        setSearchValue={setSearchValue}
-      />
-
-      <TodoList>
-        { error && <p>Desespérate, hubo un error...</p> }
-        { loading && <p>Estamos cargando, no desesperes...</p> }
-        { (!loading && !searchedTodos.length) && <p>Crea tu primer TODO</p> }
-  
-        {searchedTodos.map((todo, index) => (
-          <TodoItem 
-            key={index}
-            text={todo.text}
-            completed={todo.completed} 
-            onComplete={() => toggleTodoCompletion(todo.text)}
-            onDelete={() => deleteTodo(todo.text)}
-            />
-        ))}
-      </TodoList>
+      <TodoContext.Consumer>
+        {({ error, loading, searchedTodos, toggleTodoCompletion, deleteTodo }) => ( //** {(value) => ( */}
+          <TodoList>
+            { error && <p>Desespérate, hubo un error...</p> }
+            { loading && <p>Estamos cargando, no desesperes...</p> }
+            { (!loading && !searchedTodos.length) && <p>Crea tu primer TODO</p> }
+      
+            {searchedTodos.map((todo, index) => (
+              <TodoItem 
+                key={index}
+                text={todo.text}
+                completed={todo.completed} 
+                onComplete={() => toggleTodoCompletion(todo.text)}
+                onDelete={() => deleteTodo(todo.text)}
+                />
+            ))}
+          </TodoList>
+        )}
+      </TodoContext.Consumer>
 
       <CreateTodoButton />
     </React.Fragment>
