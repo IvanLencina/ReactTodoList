30c73cfe580d51ff09b1c5aef8f4bfcc7e10eebb
Ivan Lencina <colo9311@gmail.com>
2021-09-18 20:11:35 -0300

    comments added.

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,6 +1,14 @@
 import { useState, useEffect } from "react";
 
 /**
+ * Como se usa el hook useEffect:
+ * 
+ * Si no le pasamos ningun parametro, se ejecuta cada vez que actualizamos el estado.
+ * Si le pasamos un array vacio [] se ejecuta solamente antes de renderizar la primera vez y despues ya no.
+ * Si le pasamos una referencia, escucha eso. Por ejemplo le pasamos totalTodos y cada vez que haya cambios en totalTodos, se va a ejecutar.
+ */
+
+/**
  * Este Hook simula una llamada a una api. Por eso tiene un timeout.
  * 
  * Se usa useEffect para encapsular esta simulacion. Lo que hacemos es crear un estado loading en true y que luego de 1s, corra el codigo para obtener los datos
@@ -12,6 +20,11 @@ import { useState, useEffect } from "react";
  * La funcion saveItem permanece afuera del useEffect porque no va a ser ejecutada por el useEffect automaticamente. Es un funcion que llamamos cuando lo deseamos.
  * 
  * Por ultimo, al necesitar devolver mas datos que el estandar en un hook (2 parametros), se devuelve un objeto por convencion de React.
+ * 
+ * RESUMEN DE FUNCIONAMIENTO:
+ * Inicializamos el estado item con un valor inicial.
+ * Una vez ejecutado el timeout, obtiene los datos de localstorage, osea nuestra "api", para luego
+ * actualizar ese estado item.
  */
 
 function useLocalStorage(itemName, initialValue) {
