87afda28e280ccbe9f0deab1a63d0b1a0405eeee
Ivan Lencina <colo9311@gmail.com>
2021-09-19 17:35:08 -0300

    Modal component created.

---
+++ src/components/Modal/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Modal() {
+  return ReactDOM.createPortal(
+    <p>asdasdsad</p>,
+    document.getElementById("modal")
+  );
+}
