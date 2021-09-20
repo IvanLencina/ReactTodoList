67d335c6d7d61a5acba7c6d7e5a44b01bbc8bdf2
Ivan Lencina <colo9311@gmail.com>
2021-09-20 00:12:14 -0300

    TodoForm functionality implemented.

--- src/components/TodoForm/index.jsx
+++ src/components/TodoForm/index.jsx
@@ -2,22 +2,39 @@ import React from "react";
 import { TodoContext } from "../../TodoContext";
 
 function TodoForm() {
+  // Estado para almacenar el value del textarea donde creamos la tarea.
+  // se crea aca nomas y no en el context ya que es un pequeño estado que solo se usara en este TodoForm component. (no necesita ser compartido).
+  const [ newTodoValue, setNewTodoValue ] = React.useState('');
+
+  // Traemos addTodo del context para agregar todos al guardar.
+  // Traemos setIsModalOpened para cambiar el estado de la modal al hacer un submit o cancelar desde el formulario.
   const {
-    saveTodo
+    addTodo,
+    setIsModalOpened,
   } = React.useContext(TodoContext);
 
-  const onCancel = () => {
-    //TODO
+  const onChange = (event) => {
+    setNewTodoValue(event.target.value);
   }
 
-  const onSubmit = () => {
-    //TODO
+  const onCancel = () => {
+    setIsModalOpened(false);
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault(); // By default, a submit reloads the page. We are preventing this.
+
+    addTodo(newTodoValue); // Calling addTodo from the Context.
+
+    setIsModalOpened(false);
   }
 
   return(
     <form onSubmit={onSubmit}>
       <label>Crea un TODO</label>
-      <textarea 
+      <textarea
+        value={newTodoValue}
+        onChange={onChange}
         placeholder="Cortar la cebolla para el almuerzo"
       ></textarea>
 
