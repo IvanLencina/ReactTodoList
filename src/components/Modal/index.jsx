48794be0b944ac63214fbf2b00c7df9444f7aeac
Ivan Lencina <colo9311@gmail.com>
2021-09-19 23:39:08 -0300

    Comment added.

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./Modal.css"
 
+/**
+ * Modal fue pensada para mostrar un children. Este puede ser un componente, o cualquier tag html. 
+ * 
+ * Usamos ReactDOM.createPortal(). Este funciona parecido al render comun, pero nos permite meter codigo react de nuestra app corriendo en react,
+ * fuera de ese contexto en si, dentro de una etiqueta a parte que le definamos como segundo parametro.
+ */
 function Modal({ children }) {
   return ReactDOM.createPortal(
     <div className="ModalBackground">
